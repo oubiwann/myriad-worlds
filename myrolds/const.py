14be1e2d22fd51af7391b19e4b400d50fa02790b
Duncan McGreggor <duncan@ubuntu.com>
2011-04-26 21:35:29 -0600

    Added a constants file.

---
+++ myrolds/const.py
@@ -0,0 +1,10 @@
+# Human perceptual average distance to horizon on Earth, in miles; this is
+# taken as the unit size of a tile. To provide some reference, a small town of
+# 3000 people can easily fit in 2 square miles (that's about 1300 residential
+# buildings). So a single tile will provide pleny of space for a small town.
+# 
+# On the other extreme, a large city can be in the neighborhood of 500 square
+# miles (e.g., New York City) with a population density of 28,000 people per
+# square mile (which would be 84000 people per tile). A city the size of NYC
+# would require about 150 tiles, a grid of 10 by 15 tiles.
+horizonDistance = 3.1
