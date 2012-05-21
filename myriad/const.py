294f6e3834bdb9ba7c3e2b7cc4ab974cf01273dd
Duncan McGreggor <duncan@dreamhost.com>
2012-05-20 21:52:03 -0400

    Added signal and exit code constants (and a lookup data structure).

--- myriad/const.py
+++ myriad/const.py
@@ -1,3 +1,53 @@
+import signal
+
+
+# Signals
+SIGUSR1 = 30
+SIGUSR2 = 31
+signalLookup = {
+    signal.SIGHUP: "terminal line hangup",
+    signal.SIGINT: "interrupt program",
+    signal.SIGQUIT: "quit program",
+    signal.SIGILL: "illegal instruction",
+    signal.SIGTRAP: "trace trap",
+    signal.SIGABRT: "abort program (formerly SIGIOT)",
+    signal.SIGEMT: "emulate instruction executed",
+    signal.SIGFPE: "floating-point exception",
+    signal.SIGKILL: "kill program",
+    signal.SIGBUS: "bus error",
+    signal.SIGSEGV: "segmentation violation",
+    signal.SIGSYS: "non-existent system call invoked",
+    signal.SIGPIPE: "write on a pipe with no reader",
+    signal.SIGALRM: "real-time timer expired",
+    signal.SIGTERM: "software termination signal",
+    signal.SIGURG: "urgent condition present on socket",
+    signal.SIGSTOP: "stop (cannot be caught or ignored)",
+    signal.SIGTSTP: "stop signal generated from keyboard",
+    signal.SIGCONT: "continue after stop",
+    signal.SIGCHLD: "child status has changed",
+    signal.SIGTTIN: "background read attempted from control terminal",
+    signal.SIGTTOU: "background write attempted to control terminal",
+    signal.SIGIO: "I/O is possible on a descriptor (see fcntl(2))",
+    signal.SIGXCPU: "cpu time limit exceeded (see setrlimit(2))",
+    signal.SIGXFSZ: "file size limit exceeded (see setrlimit(2))",
+    signal.SIGVTALRM: "virtual time alarm (see setitimer(2))",
+    signal.SIGPROF: "profiling timer alarm (see setitimer(2))",
+    signal.SIGWINCH: "Window size change",
+    signal.SIGINFO: "status request from keyboard",
+    SIGUSR1: "User defined signal 1",
+    SIGUSR2: "User defined signal 2",
+}
+
+# Exit codes
+OK = 0
+GENERAL_ERROR = 1
+ILLEGAL_COMMAND = 126
+COMMAND_NOT_FOUND = 127
+INVALID_EXIT = 128
+INTERRUPTED = 128 + signal.SIGINT
+ABORTED = 128 + signal.SIGABRT
+CONTROL_D = 160
+
 # Options
 GAME_TYPE = "gametype"
 STORY_DIR = "storydir"
