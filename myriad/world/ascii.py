68046957c3288bcf5fc0b5f1a5b20736c3db908e
Duncan McGreggor <duncan.mcgreggor@rackspace.com>
2013-04-28 00:42:35 -0700

    Added ASCII to module.

---
+++ myriad/world/ascii.py
@@ -0,0 +1,199 @@
+plains = r"""
+----
+----
+--
+-
+"""
+
+savanna = r"""
+-+--
+--+-
+-+
+-
+"""
+
+woodlands = r"""
++-++
+++-+
++-
++
+"""
+
+forest = r"""
+++++
+++++
+++
++
+"""
+
+jungle = r"""
+$+&+
++&+$
+&+
+&
+"""
+
+sandy_ground = r"""
+....
+....
+..
+.
+"""
+
+desert = sandy_ground
+beach = sandy_ground
+
+rocky_ground = r"""
+,.,.
+.,.,
+,.
+,
+"""
+
+shoreline = rocky_ground
+
+hills = r"""
+^_^_
+_^_^
+^_
+^
+"""
+
+cliffs = r"""
+|,,.
+.,,|
+|,
+|
+"""
+
+caves = r"""
+^_^_
+_O_O
+_O
+o
+"""
+
+mountains = r"""
+^^^^
+^^^^
+^^
+^
+"""
+
+alpine_treeline = r"""
+^+^+
++^+^
+^+
+^
+"""
+
+high_peaks = r"""
+/\**
+**/\
+^*
+^
+"""
+
+high_plateau = r"""
+/\__
+__/\
+^_
+^
+"""
+
+valley = r"""
+\/--
+--\/
+V-
+v
+"""
+
+ravine = r"""
+-V-v
+v-V- Ravine
+V-
+v
+"""
+
+canyon = r"""
+_  _
+ \/ 
+VV
+V
+"""
+
+buttes = r"""
+...n
+n...
+n.
+n
+"""
+
+tundra = r"""
+*.*.
+.*.*
+.*
+*
+"""
+
+stream = r"""
+~S~s
+s~S~
+~s
+~
+"""
+
+river = r"""
+~{~{
+}~}~
+~S
+~
+"""
+
+lake = r"""
+~.~.
+.~.~
+~.
+~
+"""
+
+ocean = r"""
+~~~~
+~~~~
+~~
+~
+"""
+
+"""
+Old:
+
+^\/^
+/^^\ Valley (old)
+
+++++
+++++
+
+$&$&
+&$&$
+
+,.,.
+.,., RockyGround (Shoreline)
+
+....
+.... SandyGround (Desert/Beach)
+
+====
+====
+
+''''
+''''
+
+;;;;
+;;;;
+
+::::
+::::
+
+____
+____
+"""
